57c46f27365468bb69a144317171a542a1c24267
amlane <twelvexstring@gmail.com>
2019-09-01 21:31:37 -0500

    changing permissions and adding users

--- src/firebase.js
+++ src/firebase.js
@@ -24,4 +24,45 @@ export const signOut = () => auth.signOut();
 
 window.firebase = firebase;
 
+export const createUserProfileDocument = async (user, additionalData) => {
+  if (!user) return;
+
+  // Get a reference in the database where a user profile might be
+  const userRef = firestore.doc(`users/${user.uid}`);
+
+  // Go and fetch document from that location
+  const snapshot = await userRef.get();
+
+  if (!snapshot.exists) {
+    const { displayName, email, photoURL } = user;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        photoURL,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.error("error:", error);
+    }
+  }
+  return getUserDocument(user.uid);
+};
+
+export const getUserDocument = async uid => {
+  if (!uid) return null;
+  try {
+    const userDocument = await firestore
+      .collection("users")
+      .doc(uid)
+      .get();
+
+    return { uid, ...userDocument.data() };
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 export default firebaseConfig;
